8b7d1defab04625f813053d35949353c5ffe335d
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-23 18:25:08 +0800

    Update skeleton_solution.py

--- skeleton_solution.py
+++ skeleton_solution.py
@@ -6,3 +6,37 @@ rpcport=8332
 rpcip='3.134.159.30'
 
 rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpcuser, rpcpassword, rpcip, rpcport))
+
+hash=rpc_connection.getblockhash(0)
+print(hash)
+
+blockinfo=rpc_connection.getblock(hash)
+print(blockinfo)
+
+time=blockinfo.get("time")
+print(time)
+
+i=0
+for i in range(777902):
+  hash=rpc_connection.getblockhash(i)
+  blockinfo=rpc_connection.getblock(hash)
+  time=blockinfo.get("time")
+  if time>1232100000:
+    print("The block has index ", i)
+    print("The time is ", time)
+    print(blockinfo)
+    break
+  else:
+    continue
+
+#checking
+hash=rpc_connection.getblockhash(i)
+blockinfo=rpc_connection.getblock(hash)
+time1=blockinfo.get("time")
+print("Block ",i," has time", time1)
+
+hash=rpc_connection.getblockhash(i-1)
+blockinfo=rpc_connection.getblock(hash)
+time2=blockinfo.get("time")
+print("Block ",i-1," has time", time2)
+
